4b6a3bcdea9ca12bea87bc2519f3649be654a1c7
小林誠 <chocolatier@chocolat-app.com>
2018-09-09 22:58:12 +0900

    test

---
+++ test1/test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by 小林誠 on 2018/09/09.
+//  Copyright © 2018年 小林誠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
